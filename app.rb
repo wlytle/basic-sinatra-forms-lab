b0f75bb1e745da4d211aedf7aec9e3c8cf9530d2
wlytle <wlytle89@gmail.com>
2020-10-26 08:54:09 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
-require 'sinatra/base'
+require "sinatra/base"
 
 class App < Sinatra::Base
+  get "/newteam" do
+    erb :newteam
+  end
 
-
+  post "/team" do
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @point_gaurd = params[:pg]
+    @shooting_gaurd = params[:sg]
+    @small_forward = params[:sf]
+    @power_forward = params[:pf]
+    @center = params[:c]
+    erb :team
+  end
 end
